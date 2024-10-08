77f0255a03d1e7afa6c61822e4c5799e75eb7366
Alexsandro <164013020+LecoVerde@users.noreply.github.com>
2024-10-08 20:57:26 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -13,4 +13,8 @@ criaCartao(
     'o que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
-
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês se diz HI(RAI)'
+)
