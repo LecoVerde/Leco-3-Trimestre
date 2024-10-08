72a77c03fe2a63d1cd976c5460c850b474d0ea64
Alexsandro <164013020+LecoVerde@users.noreply.github.com>
2024-10-08 20:56:41 -0300

    app.js

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-criarCartao(categoria,pergunta,respota) {
+criarCartao(categoria, pergunta, resposta) ;{
 
 }
-function criacartao(categoria,pergunta,respota) {
-    let cotainer = document.getElementById('conteiner')
-    let cartao = document.createElement ('article')
+function criaCartao(categoria, pergunta, resposta){
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
     cartao.className = 'cartao'
 }
 cartao.innerHTML = 
 `<div class="cartao_conteudo"></div>
-<h3>Programação</h3>
-<div class="cartao_conteudo_pergunta">
-    <p>O que é JavaScript?</p>
-</div>
-<div class="cartao_conteudo_resposta">
-    <p>O JavaScript é uma linguagem de programação</p>
-    </div
-    </div`
+                <h3>${categoria}</h3>
+                <div class="cartao_conteudo_pergunta">
+                    <p>${pergunta}</p>
+                </div>
+                <div class="cartao_conteudo_resposta">
+                    <p>${resposta}</p>
+                    </div>
+                    </div>
+                    `
+                    container.appenchild(cartao)
+                    let respostasEstaVisivel= false
+                    function viraCartao () {
+                        respostasEstaVisivel = !respostasEstaVisivel
+                        cartao.classList.toogle('active',respostasEstaVisivel)
+                    }
+                    cartao.addEventListener('click', viraCartao)
+                    container.appenchild(cartao)
